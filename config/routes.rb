eaf4f44eeb68b8102cdbb33c60b290c9b87ddb27
Dawid <dawid.swiniarski92@gmail.com>
2021-10-22 01:46:48 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,8 @@
 # frozen_string_literal: true
 
-# Rails.application.routes.draw do
-#   scope defaults: { format: 'json' } do
-#     resources :events do
-#       post :book
-#         resources :orders do
-#           post :pay
-#         end
-#       end
-#   end
-# end
-
 Rails.application.routes.draw do
   resources :events, defaults: { format: :json } do
     member do
-      get :tickets
       post :book
       resources :orders do
         post :pay
